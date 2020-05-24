9c51868fb0b9e056eb01c9f4857564f8ddcd2a75
Joe <joegentille@gmail.com>
2020-05-23 21:09:32 -0500

    End of section 5

---
+++ API/Extensions/SwaggerServiceExtensions.cs
@@ -0,0 +1,17 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace API.Extensions
+{
+    public static class SwaggerServiceExtensions
+    {
+        public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c => 
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Skinet API", Version = "v1"} );
+            });
+            return services;
+        }
+    }
+}
